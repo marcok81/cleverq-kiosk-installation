bef0069e7fc27934137f6e762f0d6d8ccfe0301a
Marco <marco.kloehn@icloud.com>
2020-01-31 16:17:22 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo rm /home/pi/.config/autostart/chrome-afhcomalholahplbjhnmahkoekoijban-Default.desktop
+sleep 1
+sudo mv cleverq-kiosk.desktop /home/pi/.config/autostart/cleverq-kiosk.desktop
